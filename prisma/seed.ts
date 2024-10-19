e9c65425c721e2b3c2431245ef0548df0b6d7e2b
Michael Isip <michael.isip.work@gmail.com>
2024-10-19 20:30:40 +0800

    :recycle: use enums on seeder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,35 +1,35 @@
 import { PrismaClient } from '@prisma/client';
 import { faker } from '@faker-js/faker';
 import * as bcrypt from 'bcrypt';
+import { UserRole } from 'src/enums';
 
 const prisma  = new PrismaClient();
 
+const password = '123'
 const salt = Number(process.env.SALT_ROUNDS)
-const password = bcrypt.hashSync('123', salt)
+const hash = bcrypt.hashSync(password, salt)
 
 const fakerUser = (): any => ({
   firstName: faker.name.firstName(),
   lastName: faker.name.lastName(),
   email: faker.internet.email(),
-  password: password,
+  password: hash,
   roleId: faker.number.int({min: 1, max: 2}),
 });
 
 async function main() {
-  const fakerRounds = 10;
-
   console.log('Seeding...');
 
   /// --------- Roles ---------------
   await prisma.role.createMany({
     data: [
-      { name: 'user' },
-      { name: 'seller' },
+      { name: UserRole.User },
+      { name: UserRole.Seller },
     ]
   })
 
   /// --------- Users ---------------
-  for (let i = 0; i < fakerRounds; i++) {
+  for (let i = 0; i < 10; i++) {
     await prisma.user.create({ data: fakerUser() });
   }
 };
